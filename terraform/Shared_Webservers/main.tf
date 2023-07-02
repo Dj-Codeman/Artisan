b881459dcd1fda17096e8f1cd426dbb140c5da15
Darrion Whitfield <dwhitfield@ramfield.net>
2023-07-02 18:43:27 -0500

    Updated the server configs

--- terraform/Shared_Webservers/main.tf
+++ terraform/Shared_Webservers/main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.7.4"
+      # version = "2.9.11"
+      version = "2.9.10"
     }
   }
 }
@@ -16,19 +18,21 @@ provider "proxmox" {
   pm_api_token_secret = "5af5ebf3-1050-4439-a1f4-16a09eb500f0"
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = false
+  
   pm_log_enable = true
-  pm_log_file = "terraform-plugin-proxmox.log"
-#  pm_debug = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_debug      = true
   pm_log_levels = {
-    _default = "debug"
+    _default    = "debug"
     _capturelog = ""
- }
+  }
 }
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
-resource "proxmox_vm_qemu" "Shared_Machine" {
-  count = 6 # 8 clients per machine and 6 machien 8*6 = ~50 clients 50 * 20
-  name = "Artisan_Shared(${count.index + 1})" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+resource "proxmox_vm_qemu" "Shared-Machine" {
+  count = 2
+  name = "Artisan-Shared-${count.index +1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+  # ${count.index + 1}
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = var.proxmox_host
   # another variable with contents "ubuntu-2004-cloudinit-template"
@@ -36,18 +40,18 @@ resource "proxmox_vm_qemu" "Shared_Machine" {
   # basic VM settings here. agent refers to guest agent
   agent = 1
   os_type = "cloud-init"
-  cores = 2
+  cores = 4
   sockets = 1
   cpu = "host"
-  memory = 2100
-  scsihw = "virtio-scsi-pci"
+  memory = 4100
+  scsihw = "virtio-scsi-single"
   bootdisk = "scsi0"
   disk {
     slot = 0
     # set disk size here. leave it small for testing because expanding the disk takes time.
-    size = "15G"
+    size = "25G"
     type = "scsi"
-    storage = "local-lvm"
+    storage = "Vault_Disks"
     iothread = 1
   }
   
@@ -64,10 +68,8 @@ resource "proxmox_vm_qemu" "Shared_Machine" {
   }
   
   # the ${count.index + 1} thing appends text to the end of the ip address
-  ipconfig0 = "ip=10.1.0.100${count.index + 1}/24,gw=10.1.0.1"
+  ipconfig0 = "ip=10.1.0.10${count.index + 1}/24,gw=10.1.0.1"
   
   # sshkeys set using variables. the variable contains the text of the key.
-  sshkeys = <<EOF
-  ${var.ssh_key}
-  EOF
+  sshkeys = var.ssh_key
 }
